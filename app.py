b372be1e276a59d86dca85d266f67f933f18b607
ayush51163003 <ayushupatel.bscibm2024@iar.ac.in>
2025-10-19 22:43:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ for idx, story in enumerate(filtered_stories):
 
 # -------- Favorites Page --------
 if menu == "Favorites":
-    elif not st.session_state.logged_in:
+    else not st.session_state.logged_in:
         st.warning("Please login first!")
         st.stop()
 
